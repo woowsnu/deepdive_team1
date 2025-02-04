14b911b67dfa08712c094a4694af452e7f2c7092
moonsun <woowsnu@gmail.com>
2025-02-04 21:46:45 +0900

    include, text button& input 추가 작업 필요

--- js/global.js
+++ js/global.js
@@ -3,3 +3,8 @@ import { includeHTML } from './include.js';
 includeHTML("/guide/heading.html", "#heading");
 includeHTML("/guide/text.html", "#text");
 includeHTML("/guide/link.html", "#link");
+includeHTML("/guide/button.html", "#button");
+includeHTML("/guide/input.html", "#input");
+includeHTML("/guide/select.html", "#select");
+includeHTML("/guide/radio.html", "#radio");
+includeHTML("/guide/checkbox.html", "#checkbox");
