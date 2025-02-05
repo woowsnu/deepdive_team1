e5fa30198ec0e268d86328aa4f2bb890d52bec20
jinyoung-0 <sintplia@gmail.com>
2025-02-06 00:40:27 +0900

    r2

--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
-import { includeHTML } from './include.js';
+import { includeHTML } from "./include.js";
 
 includeHTML("/guide/heading.html", "#heading");
 includeHTML("/guide/text.html", "#text");
 includeHTML("/guide/link.html", "#link");
+includeHTML("/guide/button.html", "#button");
