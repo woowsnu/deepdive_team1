29cf045ee4f1b81da7e544a9d28956d037b3c799
moonsun <woowsnu@gmail.com>
2025-03-11 21:37:06 +0900

    포인트 페이지 애니메이션 적용

--- js/moon.js
+++ js/moon.js
@@ -1,5 +1,5 @@
 gsap.fromTo(
-  'div .now',
+  "div .now",
   {
     opacity: 0,
     scale: 0,
@@ -11,10 +11,41 @@ gsap.fromTo(
   }
 );
 
+// gsap.to(".sample-list", {
+//   y: "-20",
+//   ease: "expo.inOut",
+//   stagger: 0.2,
+//   duration: 1.5,
+// });
 
-gsap.to(".sample-list", {
-    y: "-20",
-    ease: "expo.inOut",
-    stagger: 0.2,
-    duration: 1.5,
-  });
+gsap.fromTo(
+  ".ani1",
+  {
+    x: -30,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    stagger: 0.15,
+    scrollTrigger: {
+      trigger: ".ani1",
+      markers: true,
+      // start: "top center",
+      start: "top 85%",
+      end: "bottom 70%",
+    },
+  },
+);
+
+// “.ani1”, {
+//   x:-30,
+// Opacity:0},{
+// X:0,
+// Opacity:1,
+// Duration1,
+// scrollTrigger: {
+// Trigger:”.ani1”,
+// Markers: true,
+// Start: “top center”,}})
